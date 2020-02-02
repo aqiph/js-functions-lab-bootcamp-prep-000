1259ef7a168e4722f83a0a5c779ca07e774f1db9
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-02 23:55:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,19 +18,3 @@ function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
 
-
-var funcs = [];
-// let's create 3 functions
-for (let i = 0; i < 3; i++) {
-  // and store them in funcs
-  console.log(i)
-  funcs[i] = function() {
-    // each should log its value.
-    console.log("My value: " + i);
-  };
-}
-//console.log(i)
-for (var j = 0; j < 3; j++) {
-  // and now let's run each one to see
-  funcs[j]();
-}
