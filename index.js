3d4bbdc38d2dc7ce3cac156d9894e66eedae7227
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-02 23:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var funcs = [];
 // let's create 3 functions
 for (let i = 0; i < 3; i++) {
   // and store them in funcs
-  //console.log(i)
+  console.log(i)
   funcs[i] = function() {
     // each should log its value.
 //    console.log(i)
